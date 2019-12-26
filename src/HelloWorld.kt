bc2d9fd72f3150cffd35a3554264dc068a674718
sameera <sameerad177@gmail.com>
2019-12-26 20:35:21 +0530

    Additional Details

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,3 +1,6 @@
 fun main(args : Array<String>) {
+    //main name of the function
+    //args is the parameter
+    //after colon there is the type of the parameter this is array pf string
     println("Hello World")
 }
